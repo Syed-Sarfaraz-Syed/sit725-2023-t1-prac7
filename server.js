f681e662019406346686aa8f08c1fbee86ba58cb
Syed-Sarfaraz-Syed <s222599005@deakin.edu.au>
2023-08-08 15:22:00 +1000

    Adding two numbers
    Adding two numbers by passing those numbers as api using express

---
+++ server.js
@@ -0,0 +1,38 @@
+let express = require('express');
+let app = express();
+
+app.use(express.static(__dirname + '/'));
+
+// Assign port to 3000
+port = 3000;
+
+app.listen(port, () => {
+    console.log("Web server running at: http://localhost:3000");
+});
+
+app.get('/', (request, response) => {
+    response.send(index.html);
+});
+
+app.get('/add', (req, res) => {
+    let status = 200;
+    let message = 'success';
+    let firstNumber = req.query.firstNumber;
+    let secondNumber = req.query.secondNumber;
+    let result = parseInt(firstNumber) + parseInt(secondNumber);
+    res.json({
+        status: status,
+        message: message,
+        data: result
+    })
+
+})
+
+
+
+
+
+
+
+
+
